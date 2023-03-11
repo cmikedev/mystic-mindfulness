b5a0a3167c9e1da72799050ec283f7182f036ade
cmikedev <carberrm@gmail.com>
2023-03-11 08:56:32 +0000

    Add: index.html to views.py and create urls.py in home app

--- mystic_mindfulness/settings.py
+++ mystic_mindfulness/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'mystic_mindfulness',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +69,9 @@ ROOT_URLCONF = 'mystic_mindfulness.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
