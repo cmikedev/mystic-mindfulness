87699a94a33cc715204cf9a512dde9b779144eac
cmikedev <carberrm@gmail.com>
2023-03-11 08:17:57 +0000

    Add: basic html and bootstrap cdn to base.html

--- mystic_mindfulness/settings.py
+++ mystic_mindfulness/settings.py
@@ -12,10 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
