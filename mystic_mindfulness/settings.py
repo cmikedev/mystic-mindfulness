96fb15bb95c4587dc3163ddd696168f902a9ccf9
cmikedev <carberrm@gmail.com>
2023-03-13 17:43:26 +0000

    Install: allauth and add settings to settings.py and urls.py

--- mystic_mindfulness/settings.py
+++ mystic_mindfulness/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary',
     'mystic_mindfulness',
     'home',
@@ -80,6 +84,16 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'mystic_mindfulness.wsgi.application'
 
