60eb47915b9350aa9bc6c19e55a3a6069a66b36f
cmikedev <carberrm@gmail.com>
2023-03-17 12:46:06 +0000

    Add: Photo model to models.py in products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Photo(models.Model):
+    """
+    This model allows for the uploading of
+    photos to Cloudinary
+    """
+    title = models.CharField(max_length=100)
+    image = CloudinaryField('image')
+
+    def __str__(self):
+        return self.title
+
+
