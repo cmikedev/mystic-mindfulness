4fd4d6f7f5c18fb1e6fac74922cca7e9fe1b61d8
cmikedev <carberrm@gmail.com>
2023-03-18 11:04:39 +0000

    Add: filtering items by category

--- products/views.py
+++ products/views.py
@@ -5,10 +5,17 @@ from .models import *
 
 
 def all_products(request):
+
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,7 +28,9 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
+
     return render(request, 'products/products.html', context)
 
 
