f376dfb49cf174dec47fbdba04ff2daa3c04149c
cmikedev <carberrm@gmail.com>
2023-04-13 12:46:29 +0000

    Add: post detail view to blog app and basic templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.view_blog, name='blog'),
+    path('', BlogView.as_view(), name='blog'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
 
 
-def view_blog(request):
-    return render(request, 'blog/blog.html', {})
+class BlogView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_details.html'
