517153e873ce8ace41be47cb07232a83614f9dc1
cmikedev <carberrm@gmail.com>
2023-04-21 14:14:39 +0000

    Edit: models.py in blog app and style post_details.html

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,12 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=300)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE
+        )
     body = models.TextField()
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
