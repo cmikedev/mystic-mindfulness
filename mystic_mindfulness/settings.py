42bcf6b76fb564603421e5aba4b38e896f746b31
cmikedev <carberrm@gmail.com>
2023-04-26 09:30:41 +0000

    Add: ckeditor configs to settings.py

--- mystic_mindfulness/settings.py
+++ mystic_mindfulness/settings.py
@@ -244,3 +244,21 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': [
+            ['Undo', 'Redo',
+             '-', 'Bold', 'Italic', 'Underline',
+             '-', 'Link', 'Unlink', 'Anchor',
+             '-', 'Format',
+             
+             '-', 'Maximize',
+             
+            ],
+        ],  
+        'width': 'auto',
+        'toolbarCanCollapse': True,
+    },
+}
