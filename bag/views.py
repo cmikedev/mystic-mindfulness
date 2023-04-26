53610a76032eede64734b4d97963725f47922bb3
cmikedev <carberrm@gmail.com>
2023-04-26 16:53:32 +0000

    Admin: reformat py files in bag app for PEP8

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, \
+    HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -11,7 +12,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-    
+
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -32,17 +33,19 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """Adjust the quantity of the specified 
+    """Adjust the quantity of the specified
        product to the specified amount"""
-    
+
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
-    
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(
+            request,
+            f'Updated {product.name} quantity to {bag[item_id]}'
+            )
     else:
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag')
