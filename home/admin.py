812622905db95c395e21bc6851ea79792df3a51c
cmikedev <carberrm@gmail.com>
2023-04-26 09:00:42 +0000

    Delete: Quote model from admin.py home app

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import Quote
-
-
-@admin.register(Quote)
-class QuoteAdmin(admin.ModelAdmin):
-    list_display = (
-        'body',
-        'author',
-        )
--- home/views.py
+++ home/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
 from .models import *
-import random
 
 
 def index(request):
-    """ A view to return the index page and random quotes """
+    """ A view to return the index page """
 
-    #quotes = list(Quote.objects.all())
-    #quotes = random.sample(quotes, 3)
-
-    quotes = Quote.objects.all()
-    quotes = Quote.objects.order_by('?')[:1]
-
-    context = {
-        'quotes': quotes,
-    }
-
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html')
